6816a0f4159d4e3aba72e5a67ab8aab74c0207de
nita <nitamaske11@gmail.com>
2025-03-14 12:17:32 +0530

    Initial comit

---
+++ demo.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    age=20;
+    string name="nita";
+    return 0;
+}
---
+++ util/utility.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+void add(int x,int y){
+    cout<<x+y;
+}
+int main(){
+    add(10,25);
+    return 0;
+}
