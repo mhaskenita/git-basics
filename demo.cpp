8a43465667a0a970f964964f182d171c19f4275c
nita <nitamaske11@gmail.com>
2025-03-18 15:50:17 +0530

    oneline

--- demo.cpp
+++ demo.cpp
@@ -3,7 +3,14 @@ using namespace std;
 void say_hello(){
     cout<<"hii everyone";
 }
+void isAdult(int age){
+    if(age>= 18){
+        cout<<"Adult";
 
+    }
+    else 
+    cout<<"minor";
+}
     int main(){
     int age;
     age=20;
